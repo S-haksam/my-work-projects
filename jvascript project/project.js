8087d767802c76ba4d7eb9ab01da19baa3e285cb
maxy256 <maxillianonlineshoeshop@gmail.com>
2025-02-03 20:47:32 +0300

    javascript trial
    a few steps into the java script

---
+++ jvascript project/project.js
@@ -0,0 +1,24 @@
+let isgirl = 'nakato';
+console.log(isgirl);
+let ismale = 'hassan';
+console.log(ismale);
+let name = 'haksam';
+console.log(name);
+ let price = 3000;
+ console.log(price);
+ const labnumber = 4000;
+console.log(labnumber);
+
+
+
+let details={
+    name:'ssaabwe haksam',
+    age:24,
+    grade:"1st class",
+    telephonenumber: '0758291294',
+};
+console.log(details);
+
+
+ 
+ 
